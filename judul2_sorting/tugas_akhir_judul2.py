afb41d5042868afe9dd8e46020588ce5f5cc5078
Afifah Naurah Alfaizah_2515061069_ SD2026 <afifahnauraha@gmail.com>
2026-05-03 17:33:07 +0700

    Update tugas_akhir_judul2.py

--- judul2_sorting/tugas_akhir_judul2.py
+++ judul2_sorting/tugas_akhir_judul2.py
@@ -1,1 +1,40 @@
+def tukar(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
+
+def bubble_sort(arr, n):
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                tukar(arr, j, j + 1)
+
+
+def main():
+    try:
+        n = int(input("4"))
+    except ValueError:
+        print("Input tidak valid!")
+        return
+    arr = []
+    print("2,4,3,5")
+    for i in range(n):
+        while True:
+            try:
+                nilai = int(input())
+                arr.append(nilai)
+                break
+            except ValueError:
+                print("Input tidak valid, silakan masukkan angka!")
+    print(f"2,4,3,5{arr}")
+    bubble_sort(arr, n)
+    print("2,3,4,5 (Bubble Sort):", end=" ")
+    for i in range(n):
+        print(arr[i], end=" ")
+    print()
+
+
+if __name__ == "__main__":
+    main()
 
